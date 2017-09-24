6bd89f9e36a47fc7eab366e3b9e8c0ac46eaeea3
webianks <webianks@gmail.com>
2017-09-24 10:58:45 +0530

    Now we have the right SDK

--- app/src/main/java/com/webianks/creddit/MainActivity.java
+++ app/src/main/java/com/webianks/creddit/MainActivity.java
@@ -1,21 +1,14 @@
 package com.webianks.creddit;
 
-
-import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import com.cloudant.sync.documentstore.AttachmentException;
-import com.cloudant.sync.documentstore.ConflictException;
-import com.cloudant.sync.documentstore.DocumentBodyFactory;
-import com.cloudant.sync.documentstore.DocumentNotFoundException;
-import com.cloudant.sync.documentstore.DocumentRevision;
-import com.cloudant.sync.documentstore.DocumentStore;
-import com.cloudant.sync.documentstore.DocumentStoreException;
+import com.cloudant.client.api.ClientBuilder;
+import com.cloudant.client.api.CloudantClient;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,34 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Obtain storage path on Android
-        File path = getApplicationContext().getDir("documentstores", Context.MODE_PRIVATE);
+        new CloudantClientAsync().execute();
 
-        try {
-            // Obtain reference to DocumentStore instance, creating it if doesn't exist
-            DocumentStore ds = DocumentStore.getInstance(new File(path, "creddit_document_store"));
+    }
 
-            // Create a document
-            DocumentRevision revision = new DocumentRevision();
-            Map<String, Object> body = new HashMap<String, Object>();
-            body.put("animal", "cat");
-            revision.setBody(DocumentBodyFactory.create(body));
-            DocumentRevision saved = ds.database().create(revision);
 
-            DocumentRevision updated = ds.database().update(saved);
+    class CloudantClientAsync extends AsyncTask<Void,Void,Void>{
 
-            // Read a document
-            DocumentRevision aRevision = ds.database().read(updated.getId());
-            aRevision.getAttachments();
+        @Override
+        protected Void doInBackground(Void... voids) {
 
-        } catch (DocumentStoreException dse) {
-            System.err.println("Problem opening or accessing DocumentStore: "+dse);
-        } catch (DocumentNotFoundException e) {
-            e.printStackTrace();
-        } catch (AttachmentException e) {
-            e.printStackTrace();
-        } catch (ConflictException e) {
-            e.printStackTrace();
+            CloudantClient client = ClientBuilder.account(getString(R.string.cloudantUsername1))
+                    .username(getString(R.string.cloudantUsername1))
+                    .password(getString(R.string.cloudantPassword1))
+                    .build();
+
+            List<String> databases = client.getAllDbs();
+            Log.d("webi","All my databases : ");
+            for ( String db : databases ) {
+                Log.d("webi",db);
+            }
+            return null;
         }
+
     }
 }
