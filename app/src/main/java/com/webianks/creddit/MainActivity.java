8f74da41fe95efbe4f5b44902e84675172e723cb
webianks <webianks@gmail.com>
2017-09-24 12:26:05 +0530

    Talking to database.

--- app/src/main/java/com/webianks/creddit/MainActivity.java
+++ app/src/main/java/com/webianks/creddit/MainActivity.java
@@ -3,24 +3,39 @@ package com.webianks.creddit;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.cloudant.client.api.ClientBuilder;
 import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView balance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+
         new CloudantClientAsync().execute();
 
     }
 
+    private void initViews() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        balance = (TextView) findViewById(R.id.main_balance);
+
+    }
 
     class CloudantClientAsync extends AsyncTask<Void,Void,Void>{
 
@@ -37,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
             for ( String db : databases ) {
                 Log.d("webi",db);
             }
+
+            Database users_db = client.database("users", false);
+            User user = users_db.find(User.class,"ramankit_1214353AB");
+            Log.d("webi",user.getName());
+
             return null;
         }
 
---
+++ app/src/main/java/com/webianks/creddit/User.kt
@@ -0,0 +1,6 @@
+package com.webianks.creddit
+
+/**
+ * Created by R Ankit on 24-09-2017.
+ */
+data class User(val _id: String,val _rev: String, val name: String) {}
